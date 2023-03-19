a68f9c81e9936153dda6289a9f3147ad70d03a10
Yousef Adel <yousefjooo2018@gmail.com>
2023-03-19 14:59:49 +0200

    init

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $allPosts = [
+            [
+                'id' => 1,
+                'title' => 'Laravel',
+                'posted_by' => 'Ahmed',
+                'created_at' => '2022-08-01 10:00:00'
+            ],
+
+            [
+                'id' => 2,
+                'title' => 'PHP',
+                'posted_by' => 'Mohamed',
+                'created_at' => '2022-08-01 10:00:00'
+            ],
+
+            [
+                'id' => 3,
+                'title' => 'Javascript',
+                'posted_by' => 'Ali',
+                'created_at' => '2022-08-01 10:00:00'
+            ],
+        ];
+
+        return view('posts.index', ['posts' => $allPosts]);
+    }
+
+    public function show($id)
+    {
+
+        $post =  [
+            'id' => 3,
+            'title' => 'Javascript',
+            'posted_by' => 'Ali',
+            'created_at' => '2022-08-01 10:00:00',
+            'description' => 'hello description',
+        ];
+
+
+
+        return view('posts.show', ['post' => $post]);
+    }
+}
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,31 @@
+@extends('layout.app')
+
+@section('content')
+
+
+<div class="card">
+    <div class="card-header">
+      post info
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+
+
+
+
+  
+  <div class="card">
+    <div class="card-header">
+      post creator info
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+@endsection
